4278e88f788b2dbb65a0cc0ddea7f214f8b29261
loicleguen <loic.leguen@hotmail.fr>
2026-04-29 09:44:46 +0200

    Add sandpiles functionality with grid stability check and summation

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,71 @@
+#include "sandpiles.h"
+#include <stdio.h>
+
+static int is_stable(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            if (grid[i][j] > 3)
+                return 0;
+    return 1;
+}
+
+static void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+    int copy[3][3];
+
+    /* addition élément par élément */
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            grid1[i][j] += grid2[i][j];
+
+    /* tant que non stable, afficher et faire basculement simultané */
+    while (!is_stable(grid1))
+    {
+        printf("=\n");
+        print_grid(grid1);
+
+        /* copie de l'état actuel pour traiter les topplings simultanés */
+        for (i = 0; i < 3; i++)
+            for (j = 0; j < 3; j++)
+                copy[i][j] = grid1[i][j];
+
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+                if (copy[i][j] > 3)
+                {
+                    grid1[i][j] -= 4;
+                    if (i - 1 >= 0)
+                        grid1[i - 1][j] += 1;
+                    if (i + 1 < 3)
+                        grid1[i + 1][j] += 1;
+                    if (j - 1 >= 0)
+                        grid1[i][j - 1] += 1;
+                    if (j + 1 < 3)
+                        grid1[i][j + 1] += 1;
+                }
+            }
+        }
+    }
+}
---
+++ sandpiles/README.md
@@ -0,0 +1,34 @@
+<div align="center"><img src="https://github.com/ksyv/holbertonschool-web_front_end/blob/main/baniere_holberton.png"></div>
+
+# Resources
+
+## Table of Contents :
+
+  - [0. Sandpiles sum](#subparagraph0)
+
+## Resources
+### Read or watch:
+* [Sandpiles - Numberphile](/rltoken/6Ft_wbSkMejwmfJQQjISlw)
+
+## Requirements
+### General
+* Allowed editors:vi,vim,emacs
+* All your files will be compiled on Ubuntu 14.04 LTS
+* Your programs and functions will be compiled withgcc 4.8.4using the flags-Wall-Werror-Wextraand-pedantic
+* All your files should end with a new line
+* Your code should use theBettystyle. It will be checked usingbetty-style.plandbetty-doc.pl
+* You are not allowed to use global variables
+* No more than 5 functions per file
+* In the following examples, themain.cfiles are shown as examples. You can use them to test your functions, but you don't have to push them to your repo (if you do we won't take them into account). We will use our ownmain.cfiles at compilation. Ourmain.cfiles might be different from the one shown in the examples
+* The prototypes of all your functions should be included in your header file calledsandpiles.h
+* Don't forget to push your header file
+* All your header files should be include guarded
+
+## Task
+### 0. Sandpiles sum <a name='subparagraph0'></a>
+
+---
+
+
+## Authors
+loicleguen - [GitHub Profile](https://github.com/loicleguen)
